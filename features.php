559791dd5f7c942c243d99797b258cf70789b130
Alexandros <alexis_iwsifidis@hotmail.com>
2019-05-17 10:46:27 +0300

    changed

--- features.php
+++ features.php
@@ -1,6 +1,41 @@
 <?php
 require_once(dirname(__FILE__) . '/features.php');
-$files = glob("media/*.*"); ?>
+
+//$files = glob("media/*.*");
+//echo '<pre>';
+//var_dump(glob('*'));
+//var_dump($files);
+//echo "<pre>";
+//print_r($files);
+//echo "</pre>";
+
+$folders = array_filter(glob('media/*'), 'is_dir');
+
+?>
+<?php if ($folders) : ?>
+<div id="category">
+
+    <?php foreach ($folders as $show): ?>
+        <?php echo "<br>"; ?>
+        <li class="tab-link data-tab=tab">
+            <a href="<?php echo $show ?>"><?= $show ?></a>
+
+        </li>
+        <?php
+        echo(count(glob("$show/*")));
+        foreach ((glob("$show/*")) as $value) {
+
+            ?>
+            <a target="_blank" rel="noopener noreferrer" href="<?php echo $value ?>"><?= $value ?>></a>
+            <?php
+        }
+        ?>
+    <?php endforeach; ?>
+    <?php endif; ?>
+</div>
+<?php ?>
+
+<?php $files = glob("media/*.*"); ?>
 <?php if ($files): ?>
     <?php for ($i = 0; $i < count($files); $i++): ?>
         <?php $file = $files[$i]; ?>
@@ -14,3 +49,8 @@ $files = glob("media/*.*"); ?>
 <?php endif; ?>
 
 
+<script>
+    $(document).ready(function () {
+        $("#category").tabs();
+    });
+</script>
